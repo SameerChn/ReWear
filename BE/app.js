159ad05882815bbffb59734b4777af1c3664e320
SameerChn <chauhan.sameer3101@gmail.com>
2025-07-23 02:09:56 +0530

    deploying using render

--- BE/app.js
+++ BE/app.js
@@ -15,10 +15,7 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 connectDB();
-app.use(cors({
-  origin: ['https://rewear-beta.vercel.app'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
